e3f65d046c3396dd72f51f04703da928e2543348
Ryan Abernathey <ryan.abernathey@gmail.com>
2020-12-04 22:52:04 -0500

    first draft

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <section class="hero is-primary is-bold">
+      <!-- Hero head: will stick at the top -->
+      <div class="hero-head">
+        <nav class="navbar">
+          <div class="container">
+            <div class="navbar-brand">
+              <a class="navbar-item">
+                <!-- <img src="https://bulma.io/images/bulma-type-white.png" alt="Logo"> -->
+              </a>
+              <span class="navbar-burger burger" data-target="navbarMenuHeroA">
+                <span></span>
+                <span></span>
+                <span></span>
+              </span>
+            </div>
+            <div id="navbarMenuHeroA" class="navbar-menu">
+              <div class="navbar-end">
+                <a class="navbar-item is-active">
+                  Home
+                </a>
+                <a class="navbar-item">
+                  Recipes
+                </a>
+                <a class="navbar-item">
+                  Catalog
+                </a>
+                <a class="navbar-item">
+                  Documentation
+                </a>
+                <span class="navbar-item">
+                  <a class="button is-primary is-inverted">
+                    <span class="icon">
+                      <i class="mdi mdi-github mdi-24px"></i>
+                    </span>
+                    <span>Repo</span>
+                  </a>
+                </span>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <img id="logo" alt="Pangeo Forage Logo" src="./assets/pangeo-forge-logo-white.png">
+          <h2 class="subtitle">
+            A cloud-native data repository for ocean, weather, and climate science.
+          </h2>
+        </div>
+        <SearchBar />
+      </div>
+    </section>
+    <Stats />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Stats from './components/Stats.vue'
+import SearchBar from './components/SearchBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Stats,
+    SearchBar
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+@import "./scss/pangeo-style.scss";
+
+// banner image
+img#logo {
+  margin-bottom: 20px;
 }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import '@mdi/font/css/materialdesignicons.css' // <-- Just here
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
 import App from './App.vue'
+
+Vue.use(VueRouter)
+Vue.use(Buefy)
 
 Vue.config.productionTip = false
 
