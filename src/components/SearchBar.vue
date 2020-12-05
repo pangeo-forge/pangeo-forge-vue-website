f86b807a84c207ecb3888a0d69eb026f4da08989
Ryan Abernathey <ryan.abernathey@gmail.com>
2020-12-04 22:53:19 -0500

    added asssets

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="columns pt-6">
+    <div class="column"></div>
+    <div class="column">
+        <p class="content"><b>Selected:</b> {{ selected }}</p>
+        <b-field>
+            <b-autocomplete
+                rounded
+                v-model="name"
+                :data="filteredDataArray"
+                placeholder="e.g. SST"
+                icon="magnify"
+                clearable
+                @select="option => selected = option">
+                <template slot="empty">No results found</template>
+            </b-autocomplete>
+        </b-field>
+    </div>
+    <div class="column"></div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'SearchBar',
+        data() {
+            return {
+                data: [
+                    'NOAA OISST',
+                    'NASA MUR SST'
+                ],
+                name: '',
+                selected: null
+            }
+        },
+        computed: {
+            filteredDataArray() {
+                return this.data.filter((option) => {
+                    return option
+                        .toString()
+                        .toLowerCase()
+                        .indexOf(this.name.toLowerCase()) >= 0
+                })
+            }
+        }
+    }
+</script>
---
+++ src/components/Stats.vue
@@ -0,0 +1,47 @@
+<template>
+  <section>
+    <nav id="stats" class="level">
+      <div class="level-item has-text-centered">
+        <div>
+          <p class="heading">Recipes</p>
+          <p class="title has-text-link">{{ recipes }}</p>
+        </div>
+      </div>
+      <div class="level-item has-text-centered">
+        <div>
+          <p class="heading">Datasets</p>
+          <p class="title has-text-link">{{ datasets }}</p>
+        </div>
+      </div>
+      <div class="level-item has-text-centered">
+        <div>
+          <p class="heading">Contributors</p>
+          <p class="title has-text-link">{{ contributors }}</p>
+        </div>
+      </div>
+    </nav>
+  </section>
+</template>
+
+
+<script>
+export default {
+  name: 'Stats',
+  data: function () {
+    return {
+      recipes: 0,
+      datasets: 0,
+      contributors: 0
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+// info bar
+nav#stats {
+  padding-top: 20px;
+}
+
+</style>
