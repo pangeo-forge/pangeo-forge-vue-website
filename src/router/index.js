e43e21fbc69d7d8cc930d925e4358a8ade21d6a4
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-07-27 16:43:42 -0700

    add Browse component to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../components/Home.vue'
+import Browse from '../components/Browse.vue'
 // import NotFound from '../components/NotFound.vue'
 
 Vue.use(VueRouter)
@@ -9,6 +10,10 @@ const routes = [
   {
     path: '/',
     component: Home
+  },
+  {
+      path: '/catalog',
+      component: Browse
   }/*,
   {
     path: '*',
