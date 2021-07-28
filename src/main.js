9544f7aaee0f0564d27beba0e93c80c4922fe727
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-07-28 10:19:32 -0700

    per stacindex, use axios for HTTP client; url is a placeholder

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
-// import VueRouter from 'vue-router'
 import router from './router'
+import axios from 'axios'
 import '@mdi/font/css/materialdesignicons.css' // <-- Just here
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
@@ -10,10 +10,17 @@ import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 import App from './App.vue'
 
-// Vue.use(VueRouter)
+// const URL = (HTTPS ? 'https://' : 'http://') + HTTP_HOST + (!HTTPS && HTTP_PORT != 80 ? ':' + HTTP_PORT : '') + HTTP_PATH;
+const URL = 'https://raw.githubusercontent.com/cisaacstern/pangeo-forge-catalog/adjust-for-browser/stac/swot-adac/GIGATL/Region01/surface/fma.json'
+
 Vue.use(Buefy)
 
 Vue.config.productionTip = false
+
+Vue.prototype.$axios = axios.create({
+  baseURL: URL,
+  timeout: 10000,
+});
 
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
