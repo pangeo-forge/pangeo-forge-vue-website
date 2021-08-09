c09f9cca6460b7f3d6231e1e8deaca763ce10e52
Ryan Abernathey <ryan.abernathey@gmail.com>
2021-08-09 19:47:11 +0200

    add comment

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
+<!--
+We try to follow this formula for layouts
+https://markus.oberlehner.net/blog/dynamic-vue-layout-components/
+-->
+
 <template>
   <component :is="layout">
     <router-view :layout.sync="layout"/>
